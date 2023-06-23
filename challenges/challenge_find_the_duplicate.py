d5081666c4b71a6956552c1c600e8470b90457c5
EduardoL-s <eduardoluizmm@hotmail.com>
2023-06-23 18:24:51 -0300

    Requisito 5 - Completo

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,18 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (len(nums) <= 0):
+        return False
+
+    numbers = nums
+    numbers.sort()
+
+    duplicated = 0
+
+    for number in range(len(numbers)):
+        if (type(numbers[number]) != int or numbers[number] < 0):
+            return False
+        if (number == len(numbers) - 1):
+            if (duplicated != 0):
+                return duplicated
+            return False
+        if (numbers[number] == numbers[number + 1]):
+            duplicated = numbers[number]
