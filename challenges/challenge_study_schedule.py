1122ead2079cc44db4a091988145eae3dec2d98f
EduardoL-s <eduardoluizmm@hotmail.com>
2023-06-23 14:46:09 -0300

    Requisito 1 - Completo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (type(target_time) != int):
+        return None
+    
+    students = list(range(len(permanence_period)))
+    total = 0
+    for item in students:
+        if (type(permanence_period[item][0]) != int or type(permanence_period[item][1]) != int):
+            return None
+
+        if (target_time in list(range(permanence_period[item][0], permanence_period[item][1] + 1))):
+            total += 1
+
+    return total
