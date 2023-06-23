c964ba84465dfa1650b4151efbb25343e2e1319d
EduardoL-s <eduardoluizmm@hotmail.com>
2023-06-23 15:45:05 -0300

    Requisito 2 - Completo

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
-
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('testando', 2) == 'odnats_et'
+
+    assert encrypt_message('testando', 3) == 'set_odnat'
+
+    assert encrypt_message('testando', 10) == 'odnatset'
+
+    with pytest.raises(TypeError, match="tipo inválido para key"): encrypt_message('testando', 'dois')
+    with pytest.raises(TypeError, match="tipo inválido para message"): encrypt_message(['testando'], 2)
