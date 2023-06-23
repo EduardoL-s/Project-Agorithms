e071269099a50539c7c80f64cacdc1f46d9ae8b6
EduardoL-s <eduardoluizmm@hotmail.com>
2023-06-23 18:29:55 -0300

    Correções de lint

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -10,9 +10,8 @@ def find_duplicate(nums):
     for number in range(len(numbers)):
         if (type(numbers[number]) != int or numbers[number] < 0):
             return False
-        if (number == len(numbers) - 1):
-            if (duplicated != 0):
-                return duplicated
-            return False
+        if (number == len(numbers) - 1 and duplicated != 0):
+            return duplicated
         if (numbers[number] == numbers[number + 1]):
             duplicated = numbers[number]
+    return False
