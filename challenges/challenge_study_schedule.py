0e2aeb4f8e52adb8344c615095dfab5cb33d3821
EduardoL-s <eduardoluizmm@hotmail.com>
2023-06-23 14:55:07 -0300

    Correção de lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,12 +4,13 @@ def study_schedule(permanence_period, target_time):
     students = list(range(len(permanence_period)))
     total = 0
     for item in students:
-        if (type(permanence_period[item][0]) != int
-            or type(permanence_period[item][1]) != int):
+        register = permanence_period[item]
+        if (type(register[0]) != int
+            or type(register[1]) != int):
             return None
 
         if (target_time in
-             list(range(permanence_period[item][0], permanence_period[item][1] + 1))):
+         list(range(register[0], register[1] + 1))):
             total += 1
 
     return total
