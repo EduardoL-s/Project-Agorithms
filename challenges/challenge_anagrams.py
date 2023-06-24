351b4bf65bd5d6728508faa64bced565e6f4d297
EduardoL-s <eduardoluizmm@hotmail.com>
2023-06-23 22:36:23 -0300

    Correção de lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -4,6 +4,7 @@ def quickSort(array, low_index, high_index):
         quickSort(array, low_index, p - 1)
         quickSort(array, p + 1, high_index)
 
+
 def partition(array, lowIndex, highIndex):
     pivot = array[highIndex]
     delimiter = lowIndex - 1
@@ -13,20 +14,20 @@ def partition(array, lowIndex, highIndex):
             delimiter = delimiter + 1
             array[index], array[delimiter] = array[delimiter], array[index]
 
-    array[delimiter + 1], array[highIndex] = array[highIndex], array[delimiter + 1]
+    array[delimiter + 1], array[highIndex] = array[highIndex],
+    array[delimiter + 1]
 
     return delimiter + 1
 
 
 def is_anagram(first_string, second_string):
-    if (len(first_string) == 0 and len(second_string) == 0 ):
+    if (len(first_string) == 0 and len(second_string) == 0):
         return (first_string, second_string, False)
     letras1 = list(first_string.lower())
     letras2 = list(second_string.lower())
-    quickSort(letras1, 0, len(letras1) - 1) 
+    quickSort(letras1, 0, len(letras1) - 1)
     quickSort(letras2, 0, len(letras2) - 1)
     palavra1 = "".join(map(str, letras1))
     palavra2 = "".join(map(str, letras2))
     return (palavra1, palavra2, palavra1 == palavra2)
-
 # código do quickSort disponível em: https://app.betrybe.com/
